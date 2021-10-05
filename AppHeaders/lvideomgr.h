7e61d8570d324486af27863ec86b74835164f3c1
Amphos <79036198+Five-Damned-Dollarz@users.noreply.github.com>
2021-10-06 09:00:39 +1100

    LVideoMgr updated with changes based on AVP2's SDK

--- AppHeaders/lvideomgr.h
+++ AppHeaders/lvideomgr.h
@@ -1,19 +1,21 @@
 #ifndef __LVIDEOMGR_H__
 #define __LVIDEOMGR_H__
 
+class VideoInst;
+typedef VideoInst* HVIDEO;
+
 class LVideoMgr
 {
-	// I'm guessing that pUnk1 in all of these functions is a video handle of some sort
-	virtual DRESULT StartOnScreenVideo(void *pUnk1, void *unk2, void *unk3)=0;
-	virtual DRESULT StartTextureVideo(void *pUnk1, void *unk2, void *unk3)=0;
+	virtual DRESULT StartOnScreenVideo(char *pFilename, uint32 flags, HVIDEO &hVideo)=0;
+	virtual DRESULT StartTextureVideo(char *pFilename, uint32 flags, HVIDEO &hVideo)=0;
 
 	// The following 3 functions are almost identical except they call a single different function after an inner loop
-	virtual DRESULT UpdateVideo(void *pUnk1)=0;
-	virtual DRESULT UpdateVideo(void *pUnk1)=0;
-	virtual DRESULT UpdateVideo(void *pUnk1)=0;
+	virtual DRESULT UpdateVideo(HVIDEO hVideo)=0;
+	virtual DRESULT UpdateVideo(HVIDEO hVideo)=0; // GetVideoStatus?
+	virtual DRESULT UpdateVideo(HVIDEO hVideo)=0; // StopVideo?
 
 	// Just guessing the second argument is a HPOLY for now
-	virtual DRESULT BindTextureToVideoPoly(void *pUnk1, HPOLY hPoly)=0;
+	virtual DRESULT BindTextureToVideoPoly(HVIDEO hVideo, HPOLY hPoly)=0;
 }
 
 #endif // __LVIDEOMGR_H__
